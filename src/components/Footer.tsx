5a25ee0dd8d1733e05fd8868dcde8649346ff8c8
codeyron <92419200+Nirshat@users.noreply.github.com>
2023-08-17 21:58:27 +0800

    modified

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,10 @@
 
+import { BsFillSuitHeartFill } from 'react-icons/bs'
 
 const Footer = () => {
   return (
     <div className="footerBox">
-      Designed & Built by Aron Paul Gonzales 💚 
+      Designed & Built by Aron Paul Gonzales <BsFillSuitHeartFill color="#0fa50f" />
     </div>
   )
 }
